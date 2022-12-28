d6064aa90b5efd3f7229a194625207bf7b320caf
Bekir ŞAFAK <bekir_safak_38@hotmail.com>
2022-12-28 21:13:09 +0300

    JS değişkenleri tanımlandı.

---
+++ main.js
@@ -0,0 +1,4 @@
+const card = document.getElementsByClassName('card');
+const addBTN = document.getElementsByClassName("btn-success");
+const cardBTN = document.querySelector('.btn-cart');
+const cardList = document.querySelector('.shopping-cart-list')
